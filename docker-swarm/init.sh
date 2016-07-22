1a4e7aff0aff55932bb1f3724ed99ddbe615aaf7
shackit <hackett_s@hotmail.com>
2016-07-22 19:00:13 +0100

    init docker swarm

---
+++ docker-swarm/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Update:
+sudo yum -y update
+
+# Install Docker - 1.12.0-rc4:
+curl -fsSL https://test.docker.com/ | sh
+
+# Enable and start the docker daemon:
+sudo systemctl enable docker
+sudo systemctl start docker
