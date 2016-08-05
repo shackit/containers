91b742ae8a0d3dbef7c26b84f8b4be325778e4fc
shackit <hackett_s@hotmail.com>
2016-08-05 22:21:08 +0100

    sets hostname, disables firewall

--- k8s/common.sh
+++ k8s/common.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 update_host() {
-  sudo yum -y clean all
   sudo yum -y update
+	sudo yum -y clean all
 
-  sudo hostname $1
+  sudo hostnamectl set-hostname $1
 
   sudo yum -y install ntp
+
+	sudo systemctl stop firewalld
+	sudo systemctl disable firewalld
 
   sudo systemctl start ntpd
   sudo systemctl enable ntpd
