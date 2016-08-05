011bad268e7f5cbdb155cb08e8865bafd270f52c
shackit <hackett_s@hotmail.com>
2016-08-05 22:25:21 +0100

    pulls stable docker 1.12

--- docker-swarm/init.sh
+++ docker-swarm/init.sh
@@ -8,7 +8,7 @@ set -o pipefail
 sudo yum -y update
 
 # Install Docker - 1.12.0-rc4:
-curl -fsSL https://test.docker.com/ | sh
+curl -fsSL https://get.docker.com/ | sh
 
 # Enable and start the docker daemon:
 sudo systemctl enable docker
