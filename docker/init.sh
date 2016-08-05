c691aed85794d56be456100c74e3af48e33ba5da
shackit <hackett_s@hotmail.com>
2016-08-05 22:24:45 +0100

    pulls custom docker image from dockerhub

--- docker/init.sh
+++ docker/init.sh
@@ -5,7 +5,6 @@ set -o nounset
 set -o pipefail
 
 # Update:
-sudo yum -y clean all
 sudo yum -y update
 
 # Download Docker some pre-requisites for compose:
@@ -16,8 +15,7 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
 # Build the docker image:
-cd /vagrant/node
-sudo docker build -t 'shackit/docker-demo' .
+sudo docker pull 'shackit/docker-demo'
 
 # Launch the docker container from image:
 sudo docker run -d -p 8080:8080 'shackit/docker-demo'
