585a86541f85f5728acf74254c9d4ea01a2abce0
shackit <hackett_s@hotmail.com>
2016-12-03 22:13:34 +0000

    make master IP variable

--- k8/init-worker.sh
+++ k8/init-worker.sh
@@ -10,14 +10,14 @@ sudo yum -y install flannel kubernetes
 echo KUBELET_ADDRESS="--address=0.0.0.0" | sudo tee /etc/kubernetes/kubelet
 echo KUBELET_PORT="--port=10250" | sudo tee -a /etc/kubernetes/kubelet
 echo KUBELET_HOSTNAME="--hostname_override=192.168.1.81" | sudo tee -a /etc/kubernetes/kubelet
-echo KUBELET_API_SERVER="--api_servers=http://192.168.1.80:8080" | sudo tee -a /etc/kubernetes/kubelet
+echo KUBELET_API_SERVER="--api_servers=http://${MASTER_IP}:8080" | sudo tee -a /etc/kubernetes/kubelet
 echo KUBELET_ARGS="" | sudo tee -a /etc/kubernetes/kubelet
 
 # Point flannel to Master IP:
-sudo sed -i 's/FLANNEL_ETCD=\"http:\/\/127.0.0.1:2379\"/FLANNEL_ETCD=\"http:\/\/192.168.1.80:2379\"/g' /etc/sysconfig/flanneld
+sudo sed -i "s/FLANNEL_ETCD=\"http:\/\/127.0.0.1:2379\"/FLANNEL_ETCD=\"http:\/\/${MASTER_IP}:2379\"/g" /etc/sysconfig/flanneld
 
 # Point kubernetes to Master IP:
-sudo sed -i 's/KUBE_MASTER=\"--master=http:\/\/127.0.0.1:8080\"/KUBE_MASTER=\"--master=http:\/\/192.168.1.80:8080\"/g' /etc/kubernetes/config
+sudo sed -i "s/KUBE_MASTER=\"--master=http:\/\/127.0.0.1:8080\"/KUBE_MASTER=\"--master=http:\/\/${MASTER_IP}:8080\"/g" /etc/kubernetes/config
 
 # Enable and restart all kuberenets related services:
 for SVC in kube-proxy kubelet docker flanneld; do
