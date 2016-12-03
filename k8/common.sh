7e320618527da0a26a43209aaa2b94eafd5619ad
shackit <hackett_s@hotmail.com>
2016-12-03 22:12:00 +0000

    formatting

--- k8/common.sh
+++ k8/common.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+export MASTER_IP=192.168.1.80
+
 update_host() {
   sudo yum -y update
-	sudo yum -y clean all
+  sudo yum -y clean all
 
   sudo hostnamectl set-hostname $1
 
   sudo yum -y install ntp
 
-	sudo systemctl stop firewalld
-	sudo systemctl disable firewalld
+  sudo systemctl stop firewalld
+  sudo systemctl disable firewalld
 
   sudo systemctl start ntpd
   sudo systemctl enable ntpd
