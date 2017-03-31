571ed5d41eb7b78b00a28299833818a4972422cb
shackit <hackett_s@hotmail.com>
2017-03-31 18:07:06 +0100

    switch to docker-compose

--- docker-jenkins/init.sh
+++ docker-jenkins/init.sh
@@ -5,21 +5,36 @@ set -o nounset
 set -o pipefail
 
 # Update:
+sudo yum -y clean all
 sudo yum -y update
 
-# Install Docker - 1.12 variant's:
-curl -fsSL https://get.docker.com/ | sh
+# Install pip & docker
+sudo yum -y install python-pip docker
+
+# Pip is out of date on CentOS7
+sudo pip install --upgrade pip
+
+# Install Docker Compose
+sudo pip install docker-compose
 
 # Enable and start the docker daemon:
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 
-# Start Jenkins
-sudo docker run -d -p 8080:8080 \
---env JAVA_OPTS="-Djenkins.install.runSetupWizard=false" \
---name=jenkins-master jenkins
+# Compose Lifecycle
+#
+# Start the stack defined in /vagrant/docker-compose.yaml
+#
+# pushd /vagrant
+# sudo docker-compose up
+#
+# Stop the stack defined in /vagrant/docker-compose.yaml
+# sudo docker-compose stop
+#
+# Removed the stack
+# sudo docker-compose rm -f
 
-# Project Blue
-sudo docker run -d -p 8888:8080 \
---env JAVA_OPTS="-Djenkins.install.runSetupWizard=false" \
---name=jenkins-blue-master jenkinsci/blueocean:latest
+# Jenkins Project Blue
+#sudo docker run -d -p 8080:8080 \
+#--env JAVA_OPTS="-Djenkins.install.runSetupWizard=false" \
+#--name=jenkins-blue-master jenkinsci/blueocean:latest
