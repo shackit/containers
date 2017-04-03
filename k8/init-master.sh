d4c5c21bb5e1469b98f452a94d1ab8b131ea778f
shackit <hackett_s@hotmail.com>
2017-04-03 11:10:52 +0100

    corrects cluster ip range

--- k8/init-master.sh
+++ k8/init-master.sh
@@ -17,7 +17,7 @@ echo KUBE_API_ADDRESS="--address=0.0.0.0" | sudo tee /etc/kubernetes/apiserver
 echo KUBE_API_PORT="--port=8080" | sudo tee -a /etc/kubernetes/apiserver
 echo KUBELET_PORT="--kubelet_port=10250" | sudo tee -a /etc/kubernetes/apiserver
 echo KUBE_ETCD_SERVERS="--etcd_servers=http://127.0.0.1:2379" | sudo tee -a /etc/kubernetes/apiserver
-echo KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=172.17.0.0/16" | sudo tee -a /etc/kubernetes/apiserver
+echo KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=172.16.0.0/16" | sudo tee -a /etc/kubernetes/apiserver
 echo KUBE_ADMISSION_CONTROL="--admission_control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ResourceQuota" | sudo tee -a /etc/kubernetes/apiserver
 echo KUBE_API_ARGS="" | sudo tee -a /etc/kubernetes/apiserver
 
